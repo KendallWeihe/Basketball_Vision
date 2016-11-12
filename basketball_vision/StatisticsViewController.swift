526b1a0e5c42074c8ecf41180c3b238195aeb347
Kendall Weihe <kendallweihe@gmail.com>
2016-11-12 10:18:09 -0500

    snapshot and setting value for statistics functional

--- basketball_vision/StatisticsViewController.swift
+++ basketball_vision/StatisticsViewController.swift
@@ -12,11 +12,69 @@ import Firebase
 
 class StatisticsViewController: UIViewController {
 
+    @IBAction func get_num_points(sender: AnyObject) {
+        print("get_num_points")
+        let users = self.ref.child("users")
+//        let user = users.child(global_email)
+//        print(user)
+        
+        users.child(global_email).observeSingleEventOfType(.Value, withBlock : {(snapShot) in
+            //print(snapShot)
+            let value = snapShot.value as! NSDictionary
+            let num_points = value["num_points"] as! NSInteger
+            print("NUM POINTS = ")
+            print(num_points)
+            
+        })
+    }
+    
+    @IBAction func get_shooting_percentage(sender: AnyObject) {
+        print("get_shooting_percetange")
+    }
+    
+    @IBAction func get_num_threes(sender: AnyObject) {
+        print("get_num_threes")
+    }
+    
+    @IBAction func get_num_twos(sender: AnyObject) {
+        print("get_num_twos")
+    }
+    
+    @IBAction func simulate_made_three(sender: AnyObject) {
+        print("simulate_made_three")
+        let users = self.ref.child("users")
+        //        let num_points = users.child(global_email).child("num_points")
+        //        print(num_points)
+        let user = users.child(global_email)
+        print(user)
+        
+        users.child(global_email).observeSingleEventOfType(.Value, withBlock : {(snapShot) in
+            
+            print(snapShot)
+            let value = snapShot.value as! NSDictionary
+            let username = value["name"] as! String ?? ""
+            let num_points = value["num_points"] as! NSInteger
+            users.child(self.global_email).child("num_points").setValue(num_points+3)
+            
+            
+        })
+    }
+    
+    @IBAction func simulate_made_two(sender: AnyObject) {
+        print("simulate_made_two")
+    }
+    
+    @IBAction func simulate_missed_shot(sender: AnyObject) {
+        print("simulate_missed_shot")
+    }
+    
+    
+    
     // [START define_database_reference]
     var ref: FIRDatabaseReference!
     // [END define_database_reference]
     
-    var email = String()
+    var global_email = String()
     var fullName = String()
     
     override func viewDidLoad() {
@@ -31,28 +89,32 @@ class StatisticsViewController: UIViewController {
         let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
         let email = appDelegate.email // email from delegate
         let firebaseEmail = email.stringByReplacingOccurrencesOfString(".", withString: "&") // makes the email valid for Firebase
+        //global_email = firebaseEmail
+        global_email = "kendallweihe@gmail&com"
         
         let fullName = appDelegate.fullName
         let userID = appDelegate.userId
         
         let newUser = ["name" : fullName,
-                       "userID" : userID]
+                       "userID" : userID,
+                       "num_points": 0,
+                       "shooting_percentage": 100,
+                       "num_threes": 0,
+                       "num_twos": 0]
         
         let users = self.ref.child("users")
-        let currentUser = users.child(firebaseEmail)
-        currentUser.setValue(newUser)
-        print(currentUser)
+        if (firebaseEmail != ""){
+            let currentUser = users.child(firebaseEmail)
+            currentUser.setValue(newUser)
+            print(currentUser)
+            
+            let unFirebaseEmailArray = firebaseEmail.componentsSeparatedByString("&")
+            let unFirebaseEmail = unFirebaseEmailArray.joinWithSeparator(".")
+            print(unFirebaseEmail) // Back to original email
+        }
+
         
-        let unFirebaseEmailArray = firebaseEmail.componentsSeparatedByString("&")
-        let unFirebaseEmail = unFirebaseEmailArray.joinWithSeparator(".")
-        print(unFirebaseEmail) // Back to original email
-        
-        
-//        let ref = FIRDatabase.database().reference()
-//        let users = ref.child("users")
-//        //let currentUser = users.childByAppendingPath(FIRAuth.auth()!.currentUser!.email!)
-//        let currentUser = users.childByAppendingPath("\(email)")
-//        print(currentUser)
+    
         
     }
 
